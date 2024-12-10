8c64707dacc19e2bf9d212ef123ebf0ab1eb264d
Jahir Castillo <jahircastilloochoa@gmail.com>
2024-12-09 18:12:10 -0600

    coalesce para mostrar un valor predeterminado si el valor de la columna es nulo

--- app/Models/ItinerarioModel.php
+++ app/Models/ItinerarioModel.php
@@ -6,7 +6,7 @@ class ItinerarioModel extends Model
     public function ponencias()
     {
         $sql   = "SELECT po_id_ponencia, po_hora_inicio, po_hora_fin,
-         concat(po_titulo, ' - ', (select nombre from ponentes where po_id_ponente = id_ponente)  )  as po_titulo,
+         concat(po_titulo, ' - ', coalesce((select nombre from ponentes where po_id_ponente = id_ponente),'') ) as po_titulo,
          (select nombre from tematicas where po_id_tematica = id_tematica) as tematica
           FROM ponencias
           WHERE po_id_convocatoria = (SELECT id_convocatoria FROM convocatorias_congresos where estatus = 'A' limit 1)
